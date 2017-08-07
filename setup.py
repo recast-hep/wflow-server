5950879392409f56a8a0cbc204e526924e34de58
lukasheinrich <lukas.heinrich@gmail.com>
2017-08-07 17:48:54 +0200

    correct setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-  name = 'wflow-controller',
+  name = 'wflow-server',
   version = '0.0.1',
   description = 'workflow server for RECAST and other workflow-based services',
   author = 'Lukas Heinrich',
   author_email = 'lukas.heinrich@cern.ch',
   packages=find_packages(),
   install_requires = [
-    'recast-celery',
+    'wflow-celery',
     'Flask',
   ],
   entry_points = {
@@ -16,6 +16,6 @@ setup(
   include_package_data = True,
   zip_safe=False,
   dependency_links = [
-    'https://github.com/recast-hep/recast-celery/tarball/master#egg=recast-celery-0.0.1'
+    'https://github.com/recast-hep/wflow-celery/tarball/master#egg=wflow-celery-0.0.1'
   ]
 )
