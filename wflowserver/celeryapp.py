95fb63f8b02dc977af6b17a3ef8f43cf026d228b
lukasheinrich <lukas.heinrich@gmail.com>
2017-12-13 00:21:15 +0100

    try out interactive

--- wflowserver/celeryapp.py
+++ wflowserver/celeryapp.py
@@ -72,7 +72,7 @@ def deploy_interactive(wflowid):
     wflowname = 'wflow-int-{}'.format(wflowid)
     deployment, service = yaml.load_all(open('/yadage_job/int_template'))
     deployment['metadata']['name'] = wflowname
-    deployment['spec']['metadata']['labels']['app'] = wflowname
+    deployment['spec']['template']['metadata']['labels']['app'] = wflowname
     service['metadata']['name'] = wflowname
     service['spec']['selector']['app'] = wflowname
 
